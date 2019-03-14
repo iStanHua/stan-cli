458b1a15cf90cf098e1ff6a89b893690204d161a
周群华 <qunhua.zhou@audaque.com>
2019-03-14 19:09:37 +0800

    potrace

--- bin/stan.js
+++ bin/stan.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const chalk = require('chalk')
 
 const pkg = require('../package.json')
 
@@ -9,6 +8,7 @@ const beautifyLib = require('../lib/beautify')
 const uglifyLib = require('../lib/uglify')
 const imageminLib = require('../lib/imagemin')
 const sassLib = require('../lib/sass')
+const potraceLib = require('../lib/potrace')
 
 program
   .version(pkg.version)
@@ -17,6 +17,7 @@ program
   .option('-u, --uglify <dir>', 'A JavaScript parser, mangler/compressor and beautifier toolkit for ES6+')
   .option('-i, --imagemin <dir> [type]', 'Minify images seamlessly')
   .option('-s, --sass <dir> [outExt]', 'Minify images seamlessly')
+  .option('-p, --potrace <dir>', ' PNG, JPEG or BMP images to SVG')
   .parse(process.argv)
 
 if (program.beautify) {
@@ -30,4 +31,7 @@ if (program.imagemin) {
 }
 if (program.sass) {
   sassLib(process.argv)
+}
+if (program.potrace) {
+  potraceLib(process.argv)
 }
